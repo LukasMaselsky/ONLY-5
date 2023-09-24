e6b9a2f02b726e17b0abd3cb216e4e1d995062e4
LukasMaselsky <lukasmaselsky@gmail.com>
2023-09-24 16:01:00 +0100

    added navbar hamburger menu

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,47 +1,99 @@
+import { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faUser, faBars } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
+import logo from "../assets/logo.png";
 
-function Navbar({ playlist }) {
+function Navbar() {
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        const handleWindowResize = () => {
+            setWindowWidth(window.innerWidth);
+        };
+        window.addEventListener("resize", handleWindowResize);
+        return () => {
+            window.removeEventListener("resize", handleWindowResize);
+        };
+    });
+
     const root = document.querySelector(":root");
     const primary = getComputedStyle(root).getPropertyValue("--primary");
     const secondary = getComputedStyle(root).getPropertyValue("--secondary");
     const accent = getComputedStyle(root).getPropertyValue("--accent");
 
+    return <>{windowWidth > 600 ? <NavNormal /> : <NavHamburger />}</>;
+}
+
+export default Navbar;
+
+function NavHamburger() {
+    const [navOpen, setNavOpen] = useState(false);
+
+    return (
+        <nav style={{ justifyContent: "space-between" }}>
+            <div
+                id="nav-logo-section"
+                className="nav-section"
+                style={{ flexGrow: "0", margin: "0" }}
+            >
+                <Link to="/">
+                    <div className="logo">
+                        <img src={logo}></img>
+                    </div>
+                </Link>
+            </div>
+            <div className="hamburger-icon-section">
+                <div className="hamburger">
+                    <FontAwesomeIcon
+                        icon={faBars}
+                        onClick={() => setNavOpen(!navOpen)}
+                    />
+                </div>
+            </div>
+            <div
+                className="hamburger-nav"
+                style={{ display: navOpen ? "block" : "none" }}
+            >
+                <div className="hamburger-nav-section">
+                    <Link to="/" className="hamburger-nav-button">
+                        <p>Home</p>
+                    </Link>
+                </div>
+                <div className="hamburger-nav-section">
+                    <Link to="/create" className="hamburger-nav-button">
+                        <p>Create</p>
+                    </Link>
+                </div>
+                <div className="hamburger-nav-section">
+                    <Link to="/explore" className="hamburger-nav-button">
+                        <p>Explore</p>
+                    </Link>
+                </div>
+                <div className="hamburger-nav-section">
+                    <Link to="/contact" className="hamburger-nav-button">
+                        <p>Contact</p>
+                    </Link>
+                </div>
+                <div className="hamburger-nav-section">
+                    <Link to="/account" className="hamburger-nav-button">
+                        <p>Account</p>
+                    </Link>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+function NavNormal() {
     return (
         <nav>
             <div id="nav-logo-section" className="nav-section">
-                <div className="logo">
-                    <h1
-                        className="logo-text"
-                        style={{
-                            color: accent,
-                            transform: "translate(1%, 0%)",
-                            zIndex: "-1",
-                        }}
-                    >
-                        ONLY 5
-                    </h1>
-                </div>
-                <div className="logo">
-                    <h1
-                        className="logo-text"
-                        style={{ color: secondary, zIndex: "-1" }}
-                    >
-                        ONLY 5
-                    </h1>
-                </div>
-                <div className="logo">
-                    <h1
-                        className="logo-text"
-                        style={{
-                            color: primary,
-                            transform: "translate(-1%, 0%)",
-                        }}
-                    >
-                        ONLY 5
-                    </h1>
-                </div>
+                <Link to="/">
+                    <div className="logo">
+                        <img src={logo}></img>
+                    </div>
+                </Link>
             </div>
             <div id="nav-home-section" className="nav-section">
                 <Link to="/" className="nav-button">
@@ -79,5 +131,3 @@ function Navbar({ playlist }) {
         </nav>
     );
 }
-
-export default Navbar;
