c3e60562c88b6168707ba1bc5ccc6f5d0fdf38d0
LukasMaselsky <lukasmaselsky@gmail.com>
2024-02-02 11:13:41 +0000

    cors origin

--- server/index.js
+++ server/index.js
@@ -3,13 +3,13 @@ import express from "express";
 import cors from "cors";
 import postRoutes from "./routes/posts.js";
 import authRoutes from "./routes/auth.js";
-import firebaseRoutes from "./routes/firebase.js"
+import firebaseRoutes from "./routes/firebase.js";
 import cookieParser from "cookie-parser";
 
 const app = express();
 app.use(express.json()); // allows to send json file using client
 app.use(cookieParser());
-app.use(cors({ origin: true, credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.static("public"));
 
 app.use("/server/posts", postRoutes);
